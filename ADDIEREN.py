4b72e6339242f29a4d9a9c030ce746caf20c8072
MiguelRS05 <miguel.rodrigues@students.lerbermatt.ch>
2023-08-31 15:33:08 +0200

    Done

--- ADDIEREN.py
+++ ADDIEREN.py
@@ -1,8 +1,3 @@
-"ADDIEREN"
-a = float(input())
-b = float(input())
-c = str(input())
-
-def addieren(c, a, b):
-    if c == "+":
-        print(a + b)
+#ADDIEREN
+def addieren(a, b):
+    return a+b
---
+++ calculator.py
@@ -0,0 +1,18 @@
+import ADDIEREN
+import dividieren
+import subtrahieren
+
+a = float(input("Write your number here :) :"))
+b = float(input("Write your second number now :) : "))
+c = str(input("Write the symbol of the desired operation :D : "))
+
+match c:
+    case "+":
+        print(ADDIEREN.addieren(a, b))
+    case "-":
+        print(subtrahieren.subtrahieren(a, b))
+    case "/":
+        print(dividieren.dividieren(a, b))
+    case other:
+        print("Not a valid thing fucko")
+
